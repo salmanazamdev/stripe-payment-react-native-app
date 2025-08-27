980b5cafd4c4e57fa8d2be9629eae724b61a8221
SalmanAzamDev <salmankhach@gmail.com>
2025-08-27 20:19:11 +0500

    Updated keys.js

---
+++ stripepaymentapp/src/config/keys.js
@@ -0,0 +1,5 @@
+export const STRIPE_KEYS = {
+  publishableKey: 'pk_test_YOUR_ACTUAL_KEY_FROM_DASHBOARD',
+};
+
+export const API_BASE_URL = 'http://10.0.2.2:3000';
--- stripepaymentapp/src/config/stripe.js
+++
@@ -1,9 +0,0 @@
-export const STRIPE_CONFIG = {
-  publishableKey: 'pk_test_YOUR_PUBLISHABLE_KEY_HERE',
-  merchantIdentifier: 'merchant.com.stripepaymentapp',
-  urlScheme: 'stripepaymentapp', // For redirects
-};
-
-export const API_BASE_URL = __DEV__ 
-  ? 'http://10.0.2.2:3000' // Android emulator
-  : 'https://your-production-api.com';
